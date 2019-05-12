845100c2af2e4fb49555ed9ca85f9c99eb88cfec
alizahidch <cmalizahid@gmail.com>
2019-05-12 19:46:47 +0500

    hourly pipe

---
+++ src/app/booking/hourlybooking.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HourlybookingPipe } from './hourlybooking.pipe';
+
+describe('HourlybookingPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HourlybookingPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
