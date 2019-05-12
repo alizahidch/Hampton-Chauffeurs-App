251de7b70d7f3ca153fc081d97edaa2a6c3e73b4
alizahidch <cmalizahid@gmail.com>
2019-05-13 02:43:07 +0500

    hourly tweak

--- src/app/booking/hourly/hourly.page.ts
+++ src/app/booking/hourly/hourly.page.ts
@@ -12,10 +12,21 @@ export class HourlyPage implements OnInit {
     2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
   ]
   private todo : FormGroup;
+  myTime: String = new Date().toTimeString();
+classes=[
+  {name:"Business", isChecked: false},
+{name:"Economy", isChecked: false},
+]
+
+
+
   constructor(private formBuilder: FormBuilder,private router:NavController) { 
     this.todo = this.formBuilder.group({
       from: ['', Validators.required],
       duration: [''],
+      date:[''],
+      time:[''],
+     class:['']
     });
   }
 
@@ -26,6 +37,22 @@ export class HourlyPage implements OnInit {
   }
   GoBack(){
     this.router.navigateBack('/booking');
+    
+  }
+
+  BookNow(){
+    console.log("function called")
+    this.classes=this.classes.filter(res=>{
+ 
+      return res.isChecked;
+    })
+    this.classes.forEach(element=>{
+    this.todo.value.class=element.name
+    })
+   
+   
+    console.log(this.todo)
+    
     
   }
 
