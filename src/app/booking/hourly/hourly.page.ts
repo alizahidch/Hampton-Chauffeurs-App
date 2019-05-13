455f62cb4e3c634410c65c64afa300c068188b8b
alizahidch <cmalizahid@gmail.com>
2019-05-14 02:54:43 +0500

    bill calculation/alertbox

--- src/app/booking/hourly/hourly.page.ts
+++ src/app/booking/hourly/hourly.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { NavController } from '@ionic/angular';
+import { NavController,AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-hourly',
   templateUrl: './hourly.page.html',
@@ -13,6 +13,11 @@ export class HourlyPage implements OnInit {
   ]
   private todo : FormGroup;
   myTime: String = new Date().toTimeString();
+economyBill:any=0;
+businessBill:any=0;
+totalBill:any=0;
+economyRate:any=50;
+businessRate:any=100;
 classes=[
   {name:"Business", isChecked: false},
 {name:"Economy", isChecked: false},
@@ -20,7 +25,7 @@ classes=[
 
 
 
-  constructor(private formBuilder: FormBuilder,private router:NavController) { 
+  constructor(private formBuilder: FormBuilder,private router:NavController,public alertController: AlertController) { 
     this.todo = this.formBuilder.group({
       from: ['', Validators.required],
       duration: [''],
@@ -40,8 +45,38 @@ classes=[
     
   }
 
+  async presentAlertConfirm(bill,cls,dat,hr) {
+    let bl=bill;
+    let cl=cls;
+    let dt=dat;
+    let h=hr;
+
+    let msg='Dear Customer your trip details are below <br> <hr> Bill :<strong>'+bl+'</strong>$'+'<br> <hr> Class:'+cls+'<br> <hr> Date:'+dat+'<br> <hr> Duration:'+hr+'hours';
+    const alert = await this.alertController.create({
+      header: 'Booking Request!',
+      message: msg,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
   BookNow(){
-    console.log("function called")
+
     this.classes=this.classes.filter(res=>{
  
       return res.isChecked;
@@ -50,10 +85,23 @@ classes=[
     this.todo.value.class=element.name
     })
    
-   
-    console.log(this.todo)
-    
-    
+   let hours;
+   hours=this.todo.value.duration.split(" ", 2)
+
+   if(this.todo.value.class=="Economy"){
+this.economyBill=this.economyRate * hours[0];
+
+   }
+   else
+   {
+  this.businessBill=this.businessRate*hours[0]
+   }
+ 
+
+   this.totalBill=this.businessBill+this.economyBill
+ 
+  
+    this.presentAlertConfirm(this.totalBill,this.todo.value.class,this.todo.value.date,hours[0])
   }
 
 }
