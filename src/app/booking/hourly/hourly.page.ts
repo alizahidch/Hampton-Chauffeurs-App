1df04bbaa40c87ff32e9c25c4c8dfc17683e27f7
alizahidch <cmalizahid@gmail.com>
2019-05-15 02:53:42 +0500

    mpboxcompo

--- src/app/booking/hourly/hourly.page.ts
+++ src/app/booking/hourly/hourly.page.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController,AlertController } from '@ionic/angular';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-hourly',
   templateUrl: './hourly.page.html',
   styleUrls: ['./hourly.page.scss'],
 })
 export class HourlyPage implements OnInit {
+
+//Map settings
+map: mapboxgl.Map;
+geocodingClient:mapboxgl.MapboxGeocoder;
+style = 'mapbox://styles/mapbox/outdoors-v9';
+lat = 40.963432;
+lng = -72.184799;
+message = 'Hello World!';
+
+
 
   time=[
     2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
@@ -18,6 +31,9 @@ businessBill:any=0;
 totalBill:any=0;
 economyRate:any=50;
 businessRate:any=100;
+
+
+
 classes=[
   {name:"Business", isChecked: false},
 {name:"Economy", isChecked: false},
@@ -36,7 +52,36 @@ classes=[
   }
 
   ngOnInit() {
+  
+    this.buildMap();
+    this.searchMap();
   }
+  
+
+  buildMap() {
+    mapboxgl.accessToken= 'pk.eyJ1IjoiYWxpemFoaWRjaCIsImEiOiJjanZvNzJvdGcxcTMzM3ptbDd5b2F5N3l2In0.Z1g7_CVa4-Nld_m4jBFsYw';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat]
+    });
+    console.log(this.map)
+  }
+
+  searchMap(){
+  console.log(this.geocodingClient)
+    this.geocodingClient.forwardGeocode({
+      query: 'Paris, France',
+      limit: 2
+    })
+      .send()
+      .then(response => {
+        const match = response.body;
+        console.log(match)
+      });
+  }
+
   logForm(){
     console.log(this.todo.value)
   }
