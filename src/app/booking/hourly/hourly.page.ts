68b303b8fecd9ea24785e2b666d74361e0a3d681
alizahidch <cmalizahid@gmail.com>
2019-05-18 16:17:22 +0500

    hourly google map component

--- src/app/booking/hourly/hourly.page.ts
+++ src/app/booking/hourly/hourly.page.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit,ViewChild ,ElementRef} from '@angular/core';
+import { Component,NgZone, OnInit,ViewChild ,ElementRef} from '@angular/core';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController,AlertController } from '@ionic/angular';
 import * as mapboxgl from 'mapbox-gl';
+import {FormControl} from "@angular/forms";
+// import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
 
 import { environment } from 'src/environments/environment';
 
 
-var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
+// var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
 
 @Component({
   selector: 'app-hourly',
@@ -14,10 +17,25 @@ var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
   styleUrls: ['./hourly.page.scss'],
 })
 export class HourlyPage implements OnInit {
+address;
 
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+  fromLocation;
+
+  @ViewChild("search")
+  public searchElementRef;
+
+  
+  
+txtHome:HTMLInputElement;
 //Map settings
 map: mapboxgl.Map;
 @ViewChild('map') mapInput:any;
+
+@ViewChild('searchTextField') input : ElementRef;
 
 
 geocodingClient=mapboxgl.places;
@@ -48,7 +66,8 @@ classes=[
 
 
 
-  constructor(private formBuilder: FormBuilder,private router:NavController,public alertController: AlertController) { 
+  constructor(private formBuilder: FormBuilder,private router:NavController,public alertController: AlertController, private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { 
     this.todo = this.formBuilder.group({
       from: ['', Validators.required],
       duration: [''],
@@ -56,15 +75,72 @@ classes=[
       time:[''],
      class:['']
     });
+
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
   }
 
   ngOnInit() {
+    this.zoom = 4;
+      this.latitude = 39.8282;
+      this.longitude = -98.5795;
+
+      //create search FormControl
+      this.searchControl = new FormControl();
+
+      //set current position
+      this.setCurrentPosition();
+
+      //load Places Autocomplete
+      this.mapsAPILoader.load().then(() => {
+        let nativeHomeInputBox = this.input.nativeElement as HTMLInputElement;
+      
+          // let nativeHomeInputBox = document.getElementById('txtHome').getElementsByTagName('input')[0] as HTMLInputElement;
+          let autocomplete = new google.maps.places.Autocomplete(nativeHomeInputBox, {
+              types: ["geocode"]
+          });
+          autocomplete.addListener("place_changed", () => {
+              this.ngZone.run(() => {
+                  //get the place result
+                  let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+                  //verify result
+                  if (place.geometry === undefined || place.geometry === null) {
+                      return;
+                  }
+
+                  //set latitude, longitude and zoom
+                  this.latitude = place.geometry.location.lat();
+                  this.longitude = place.geometry.location.lng();
+                  console.log(this.longitude);
+                  console.log(this.latitude);
+                  this.fromLocation=place.formatted_address;
+                  this.zoom = 12;
+              });
+          });
+      });
   
-    this.buildMap();
-    console.log(this.mapInput.config)
+    // this.buildMap();
+    // console.log(this.mapInput.config)
     // this.searchMap();
   }
   
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+            this.zoom = 12;
+        });
+    }
+}
 
   buildMap() {
     mapboxgl.accessToken= 'pk.eyJ1IjoiYWxpemFoaWRjaCIsImEiOiJjanZvNzJvdGcxcTMzM3ptbDd5b2F5N3l2In0.Z1g7_CVa4-Nld_m4jBFsYw';
@@ -76,30 +152,30 @@ classes=[
       center: [this.lng, this.lat]
     });
 
-    this.map.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-      query: 'Paris, France',
-      limit: 2
-      }));
-    console.log(this.map)
+    // this.map.addControl(new MapboxGeocoder({
+    //   accessToken: mapboxgl.accessToken,
+    //   mapboxgl: mapboxgl,
+    //   query: 'Paris, France',
+    //   limit: 2
+    //   }));
+    // console.log(this.map)
 
-    var geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-      });
+    // var geocoder = new MapboxGeocoder({
+    //   accessToken: mapboxgl.accessToken,
+    //   mapboxgl: mapboxgl
+    //   });
 
    
 
-   geocoder.forwardGeocode({
-      query: 'Paris, France',
-      limit: 2
-    })
-      .send()
-      .then(response => {
-        const match = response.body;
-        console.log(match)
-      });
+  //  geocoder.forwardGeocode({
+  //     query: 'Paris, France',
+  //     limit: 2
+  //   })
+  //     .send()
+  //     .then(response => {
+  //       const match = response.body;
+  //       console.log(match)
+  //     });
 
   }
 
@@ -116,13 +192,13 @@ classes=[
     
   }
 
-  async presentAlertConfirm(bill,cls,dat,hr) {
+  async presentAlertConfirm(bill,cls,dat,hr,loc,lat,lon) {
     let bl=bill;
     let cl=cls;
     let dt=dat;
     let h=hr;
 
-    let msg='Dear Customer your trip details are below <br> <hr> Bill :<strong>'+bl+'</strong>$'+'<br> <hr> Class:'+cls+'<br> <hr> Date:'+dat+'<br> <hr> Duration:'+hr+'hours';
+    let msg='Dear Customer your trip details are below <br> <hr> Bill :<strong>'+bl+'</strong>$'+'<br> <hr> Class:'+cls+'<br> <hr> Date:'+dat+'<br> <hr> Duration:'+hr+'hours <br> <hr> ' +'<br> <hr> Location:'+this.fromLocation+'<br> <hr> Lat:'+lat+'<br> <hr> Lon:'+lon;
     const alert = await this.alertController.create({
       header: 'Booking Request!',
       message: msg,
@@ -147,7 +223,7 @@ classes=[
   }
 
   BookNow(){
-
+this.fromLocation;
     this.classes=this.classes.filter(res=>{
  
       return res.isChecked;
@@ -172,7 +248,7 @@ this.economyBill=this.economyRate * hours[0];
    this.totalBill=this.businessBill+this.economyBill
  
   
-    this.presentAlertConfirm(this.totalBill,this.todo.value.class,this.todo.value.date,hours[0])
+    this.presentAlertConfirm(this.totalBill,this.todo.value.class,this.todo.value.date,hours[0],this.todo.value.from,this.latitude,this.longitude)
   }
 
 }
