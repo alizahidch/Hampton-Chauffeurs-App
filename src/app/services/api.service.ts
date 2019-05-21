482cdd147b1a801cb47d525f6628945fb9455384
alizahidch <cmalizahid@gmail.com>
2019-05-22 02:44:44 +0500

    api service initial cmp

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase} from "angularfire2/database";
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+userid;
+  constructor(public db:AngularFireDatabase) { 
+    this.userid=localStorage.getItem('user_id');
+
+  }
+
+hourlyBooking(booking:any){
+  console.log(booking);
+let bookingref:'/bookings';
+return this.db.list('/bookings').push(booking);
+
+}
+
+}
