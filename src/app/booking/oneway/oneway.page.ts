04541f91a49d3157e0840bf59c4dc638cb217704
alizahidch <cmalizahid@gmail.com>
2019-05-26 18:20:58 +0500

    owbooking

--- src/app/booking/oneway/oneway.page.ts
+++ src/app/booking/oneway/oneway.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController,AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-oneway',
   templateUrl: './oneway.page.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnewayPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:NavController) { }
 
   ngOnInit() {
+  }
+
+  GoBack(){
+    this.router.navigateBack('/booking');
+    
   }
 
 }
