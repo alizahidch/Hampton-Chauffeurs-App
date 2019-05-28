26daf9be7fa6e18851d7e112ac92c1241ea8739c
alizahidch <cmalizahid@gmail.com>
2019-05-28 23:52:02 +0500

    spc

--- src/app/booking/hourly/hourly.page.ts
+++ src/app/booking/hourly/hourly.page.ts
@@ -156,6 +156,7 @@ classes=[
     }
 }
 
+
   buildMap() {
     mapboxgl.accessToken= 'pk.eyJ1IjoiYWxpemFoaWRjaCIsImEiOiJjanZvNzJvdGcxcTMzM3ptbDd5b2F5N3l2In0.Z1g7_CVa4-Nld_m4jBFsYw';
     this.map = new mapboxgl.Map({
